0ee748bac7a21b0dd3d23f48e1de86f52d01c2ce
emm <723125@gmail.com>
2020-12-02 11:00:10 +0000

    Initial Commit

---
+++ 01_Recipe_App_UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01_Recipe_App_UI
+//
+//  Created by emm on 02/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 01_Recipe_App_UI/_1_Recipe_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_Recipe_App_UIApp.swift
+//  01_Recipe_App_UI
+//
+//  Created by emm on 02/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct _1_Recipe_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
